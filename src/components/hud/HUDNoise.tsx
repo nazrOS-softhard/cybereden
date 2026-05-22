e06e52d726a17a7675197b47316750cabb7aea25
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-05-22 12:59:21 +0300

    Update HUDNoise.tsx

--- src/components/hud/HUDNoise.tsx
+++ src/components/hud/HUDNoise.tsx
@@ -1,1 +1,8 @@
 
+export function HUDNoise() {
+  return (
+    <div className="pointer-events-none absolute inset-0 z-2">
+      <div className="h-full w-full bg-[url('/noise.png')] opacity-10 mix-blend-overlay" />
+    </div>
+  );
+}
